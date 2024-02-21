c8906fa2160f4d3bfa27170def4394644ff22415
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-21 10:12:57 -0800

    Update Connect.js

--- src/components/Connect.js
+++ src/components/Connect.js
@@ -6,7 +6,7 @@ export const Connect = function (props) {
   return (
     <React.Fragment>
       <div
-        className="font-semibold uppercase lg:text-2xl text-xl hover:text-trumpos-fontLight text-center border-yellow-300 px-2 mx-auto lg:mx-0 my-auto py-2 bg-gray-200/[0.3] hover:bg-gradient-to-r hover:from-indigo-500 hover:via-purple-500 hover:to-pink-500 text-yellow-300 items-center"
+        className="font-semibold uppercase lg:text-2xl text-xl hover:text-trumpos-fontLight text-center border-green-300 px-2 mx-auto lg:mx-0 my-auto py-2 bg-gray-200/[0.3] hover:bg-gradient-to-r hover:from-white-500 hover:via-blue-500 hover:to-green-500 text-black-300 items-center"
       >
         <ConnectWallet />
       </div>
