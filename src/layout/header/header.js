2b12cbcfa9cfcf191013e8c9203f81fca08ee4e5
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-23 08:31:43 -0800

    Update header.js

--- src/layout/header/header.js
+++ src/layout/header/header.js
@@ -1,38 +1,38 @@
 export default function Header() {
     return (
         <>
-            <a href="https://rocketpool.net/#header" className="hidden" id="#header">Header</a>
+            <a href="https://tetrausd.com/#header" className="hidden" id="#header">Header</a>
             <a
-                href="https://rocketpool.net/#stake-run-node"
+                href="https://tetrausd.com/#stake-run-node"
                 className="hidden"
                 id="#stake-run-node"
             >stake-run-node</a>
             <a
-                href="https://rocketpool.net/#security"
+                href="https://tetrausd.com//#security"
                 className="hidden"
                 id="#security"
             >Security</a>
             <a
-                href="https://rocketpool.net/#how-it-works"
+                href="https://tetrausd.com/#how-it-works"
                 className="hidden"
                 id="#how-it-works"
             >How it works</a>
             <a
-                href="https://rocketpool.net/#community"
+                href="https://tetrausd.com/#community"
                 className="hidden"
                 id="#community"
             >Community</a>
-            <a href="https://rocketpool.net/#news" className="hidden" id="#news"
+            <a href="https://tetrausd.com/#news" className="hidden" id="#news"
             >News</a>
-            <a href="https://rocketpool.net/#team" className="hidden" id="#team"
+            <a href="https://tetrausd.com/#team" className="hidden" id="#team"
             >Team</a>
-            <a href="https://rocketpool.net/#dao" className="hidden" id="#dao">Dao</a>
+            <a href="https://tetrausd.com/#dao" className="hidden" id="#dao">Dao</a>
             <a
-                href="https://rocketpool.net/#node-operator-interest"
+                href="https://tetrausd.com/#node-operator-interest"
                 className="hidden"
                 id="#node-operator-interest"
             >Node Operator Interest</a>
-            <a href="https://rocketpool.net/#footer" className="hidden" id="#footer"
+            <a href="https://tetrausd.com/#footer" className="hidden" id="#footer"
             >Footer
             </a>
         </>
