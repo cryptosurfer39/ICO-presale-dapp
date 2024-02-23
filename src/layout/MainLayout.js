b9afd8861f2efa85e758d63c8c46eebf8c942f76
cryptosurfer39 <52559399+cryptosurfer39@users.noreply.github.com>
2024-02-23 01:22:14 -0800

    Update MainLayout.js

--- src/layout/MainLayout.js
+++ src/layout/MainLayout.js
@@ -3,7 +3,7 @@ import Footer from "./footer/footer";
 
 export default function MainLayout(props) {
   return (
-    <div className="flex flex-col h-screen bg-gradient-to-t from-white via-blue-500 to-green-500">
+    <div className="flex flex-col h-screen bg-gradient-to-t from-white via-blue-500 to-white-500">
       <Header />
       <main className="mb-auto text-white">{props.children}</main>
       <Footer />
